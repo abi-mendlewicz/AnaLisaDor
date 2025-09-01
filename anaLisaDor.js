99bb1107a0e29ed958ce35c80989a7e01f6a4bf8
Abi Mendlewicz <abi_men@yahoo.com>
2025-09-01 17:25:56 -0300

    Agregado cálculo de fórmula.

--- anaLisaDor.js
+++ anaLisaDor.js
@@ -15,6 +15,14 @@ const ol15 = document.getElementById('ol15')
 const lmp = document.getElementById('lmp')
 const pl6 = document.getElementById('pl6')
 const pf = document.getElementById('pf')
+const lmoN = document.getElementById('lmoN')
+const ol15N = document.getElementById('ol15N')
+const lmpN = document.getElementById('lmpN')
+const pl6N = document.getElementById('pl6N')
+const pfN = document.getElementById('pfN')
+const suma = document.getElementById('suma')
+const c = document.getElementById('c')
+const idl = document.getElementById('idl')
 const detail = document.getElementById('detail')
 
 submitButton.addEventListener('click', () => {
@@ -30,7 +38,7 @@ submitButton.addEventListener('click', () => {
   let totalPalabras = 0
   let totalLetras = 0
   let max15Palabras = 0
-  let max8Letras = 0
+  let min9Letras = 0
   let max6Letras = 0
 
   frases.forEach((frase, i) => {
@@ -45,8 +53,8 @@ submitButton.addEventListener('click', () => {
         totalPalabras ++
         totalLetras += palabraLimpia.length
 
-        if (palabraLimpia.length >= 8) {
-          max8Letras ++
+        if (palabraLimpia.length >= 9) {
+          min9Letras ++
         } else if (palabraLimpia.length > 6) {
           max6Letras ++
         }
@@ -68,10 +76,10 @@ submitButton.addEventListener('click', () => {
         const p = document.createElement('p')
         let d = `${palabra}: ${palabra.length} letra${palabra.length > 1 ? 's' : ''}`
 
-        if (palabra.length >= 8) {
-          d += '<span class="ms-1 bg-red-400">>=8</span>'
+        if (palabra.length >= 9) {
+          d += '<span class="ms-1 bg-red-400">>=9</span>'
         } else if (palabra.length > 6) {
-          d += '<span class="ms-1 bg-red-200">>6</span>'
+          d += '<span class="ms-1 bg-red-200">7-8</span>'
         }
         
         p.innerHTML = d
@@ -95,7 +103,7 @@ submitButton.addEventListener('click', () => {
   tOl15.innerText = max15Palabras
   tPalabras.innerText = totalPalabras
   tPl6.innerText = max6Letras
-  tPf.innerText = max8Letras
+  tPf.innerText = min9Letras
   tLetras.innerText = totalLetras
 
   // Render métricas
@@ -107,12 +115,66 @@ submitButton.addEventListener('click', () => {
     style: 'percent',
     maximumFractionDigits: 2,
   }
+
+  const metrics = {
+    lmo: (totalPalabras / totalFrases),
+    ol15: (max15Palabras / totalFrases),
+    lmp: (totalLetras / totalPalabras),
+    pl78: (max6Letras / totalPalabras),
+    plRare: (min9Letras / totalPalabras),
+  }
+
+  const weights = {
+    lmo: 1,
+    ol15: 1.2,
+    lmp: 1,
+    pl78: 1,
+    plRare: 1.5,
+  }
   
-  lmo.innerText = (totalPalabras / totalFrases).toLocaleString('es', decimalOpts)
-  ol15.innerText = (max15Palabras / totalFrases).toLocaleString('es', percentOpts)
-  lmp.innerText = (totalLetras / totalPalabras).toLocaleString('es', decimalOpts)
-  pl6.innerText = (max6Letras / totalPalabras).toLocaleString('es', percentOpts)
-  pf.innerText = (max8Letras / totalPalabras).toLocaleString('es', percentOpts)
+  lmo.innerText = metrics.lmo.toLocaleString('es', decimalOpts)
+  ol15.innerText = metrics.ol15.toLocaleString('es', percentOpts)
+  lmp.innerText = metrics.lmp.toLocaleString('es', decimalOpts)
+  pl6.innerText = metrics.pl78.toLocaleString('es', percentOpts)
+  pf.innerText = metrics.plRare.toLocaleString('es', percentOpts)
+
+  // Calcular fórmula
+
+
+  // Normalización
+  const lmoNormalized = Math.min(1, metrics.lmo / 20)
+  const ol15Normalized = metrics.ol15 / 100
+  const lmpNormalized = Math.min(1, metrics.lmp / 8)
+  const pl78Normalized = metrics.pl78 / 100
+  const plRareNormalized = metrics.plRare / 100
+
+  // Render métricas normalizadas
+  lmoN.innerText = lmoNormalized.toLocaleString('es', decimalOpts)
+  ol15N.innerText = ol15Normalized.toLocaleString('es', percentOpts)
+  lmpN.innerText = lmpNormalized.toLocaleString('es', decimalOpts)
+  pl6N.innerText = pl78Normalized.toLocaleString('es', percentOpts)
+  pfN.innerText = plRareNormalized.toLocaleString('es', percentOpts)
+
+  // Suma ponderada e IDL
+  const sum =
+    weights.lmo * lmoNormalized +
+    weights.ol15 * ol15Normalized +
+    weights.lmp * lmpNormalized +
+    weights.pl78 * pl78Normalized +
+    weights.plRare * plRareNormalized
+
+  const tope = 3.6
+
+  suma.innerText = `${weights.lmo} * ${lmoNormalized} +
+    ${weights.ol15} * ${ol15Normalized} +
+    ${weights.lmp} * ${lmpNormalized} +
+    ${weights.pl78} * ${pl78Normalized} +
+    ${weights.plRare} * ${plRareNormalized} = ${sum.toLocaleString('es', decimalOpts)}`
+  c.innerText = tope.toLocaleString('es', decimalOpts)
+
+  const res = 100 * Math.min(1, (tope > 0 ? (sum / tope) : 0))
+
+  idl.innerText = res
 })
 
 clearTextButton.addEventListener('click', () => {
@@ -128,5 +190,8 @@ clearTextButton.addEventListener('click', () => {
   lmp.innerText = ''
   pl6.innerText = ''
   pf.innerText = ''
+  suma.innerText = ''
+  c.innerText = ''
+  idl.innerText = ''
   detail.innerHTML = '<h2 class="mt-4 text-xl">Detalle</h2>'
 })
